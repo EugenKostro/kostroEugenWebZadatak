123a5f53a3b32d127d746929ff936659b6fce6e9
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2021-01-16 18:46:32 +0100

    Add files via upload

---
+++ kostroEugenPolugodisnjiZadatak1/kostroEugenPolugodisnjiZadatak1/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace kostroEugenPolugodisnjiZadatak1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNatrag = new System.Windows.Forms.Button();
+            this.btnNaprijed = new System.Windows.Forms.Button();
+            this.btnIdi = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnGmail = new System.Windows.Forms.Button();
+            this.btnWebmail = new System.Windows.Forms.Button();
+            this.btnIspis = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNatrag
+            // 
+            this.btnNatrag.Location = new System.Drawing.Point(13, 13);
+            this.btnNatrag.Name = "btnNatrag";
+            this.btnNatrag.Size = new System.Drawing.Size(84, 34);
+            this.btnNatrag.TabIndex = 0;
+            this.btnNatrag.Text = "Natrag";
+            this.btnNatrag.UseVisualStyleBackColor = true;
+            this.btnNatrag.Click += new System.EventHandler(this.btnNatrag_Click);
+            // 
+            // btnNaprijed
+            // 
+            this.btnNaprijed.Location = new System.Drawing.Point(103, 13);
+            this.btnNaprijed.Name = "btnNaprijed";
+            this.btnNaprijed.Size = new System.Drawing.Size(81, 34);
+            this.btnNaprijed.TabIndex = 1;
+            this.btnNaprijed.Text = "Naprijed";
+            this.btnNaprijed.UseVisualStyleBackColor = true;
+            this.btnNaprijed.Click += new System.EventHandler(this.btnNaprijed_Click);
+            // 
+            // btnIdi
+            // 
+            this.btnIdi.Location = new System.Drawing.Point(487, 12);
+            this.btnIdi.Name = "btnIdi";
+            this.btnIdi.Size = new System.Drawing.Size(41, 35);
+            this.btnIdi.TabIndex = 2;
+            this.btnIdi.Text = "Idi";
+            this.btnIdi.UseVisualStyleBackColor = true;
+            this.btnIdi.Click += new System.EventHandler(this.btnIdi_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 53);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(776, 385);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(203, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Adresa:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(266, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // btnGmail
+            // 
+            this.btnGmail.Location = new System.Drawing.Point(534, 12);
+            this.btnGmail.Name = "btnGmail";
+            this.btnGmail.Size = new System.Drawing.Size(83, 35);
+            this.btnGmail.TabIndex = 6;
+            this.btnGmail.Text = "Gmail";
+            this.btnGmail.UseVisualStyleBackColor = true;
+            this.btnGmail.Click += new System.EventHandler(this.btnGmail_Click);
+            // 
+            // btnWebmail
+            // 
+            this.btnWebmail.Location = new System.Drawing.Point(623, 12);
+            this.btnWebmail.Name = "btnWebmail";
+            this.btnWebmail.Size = new System.Drawing.Size(77, 35);
+            this.btnWebmail.TabIndex = 7;
+            this.btnWebmail.Text = "Webmail";
+            this.btnWebmail.UseVisualStyleBackColor = true;
+            this.btnWebmail.Click += new System.EventHandler(this.btnWebmail_Click);
+            // 
+            // btnIspis
+            // 
+            this.btnIspis.Location = new System.Drawing.Point(706, 13);
+            this.btnIspis.Name = "btnIspis";
+            this.btnIspis.Size = new System.Drawing.Size(82, 34);
+            this.btnIspis.TabIndex = 8;
+            this.btnIspis.Text = "Ispis";
+            this.btnIspis.UseVisualStyleBackColor = true;
+            this.btnIspis.Click += new System.EventHandler(this.btnIspis_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnIspis);
+            this.Controls.Add(this.btnWebmail);
+            this.Controls.Add(this.btnGmail);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btnIdi);
+            this.Controls.Add(this.btnNaprijed);
+            this.Controls.Add(this.btnNatrag);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNatrag;
+        private System.Windows.Forms.Button btnNaprijed;
+        private System.Windows.Forms.Button btnIdi;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnGmail;
+        private System.Windows.Forms.Button btnWebmail;
+        private System.Windows.Forms.Button btnIspis;
+    }
+}
+
